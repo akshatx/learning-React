dfd5f7bbf8f71ce4e82158cfd7babecebe652b6f
Akshat Agarwal <88102754+akshatx@users.noreply.github.com>
2023-03-23 00:10:48 +0530

    Create index.js

---
+++ first react code/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+  <h1> First React code </h1>
+  <p> Hope to make great projects!! </p>
+  </div>
+  , document.getElementById('root')
+); 
